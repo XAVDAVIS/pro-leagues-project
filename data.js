3201c068a522a984cc4cdd7fe1885045211ae863
Xavier <shottyx5156@hotmail.com>
2023-01-05 08:49:28 -0500

    added 2 sports

--- data.js
+++ data.js
@@ -7,9 +7,21 @@ module.exports = [
         teams: '30'
     }, {
         name: 'NFL',
-        description: 'The NFL, short for the National Football League, is a professional football league where the best American football players in the world get to show their talents. ',
+        description: 'The NFL, short for the National Football League, is a professional American football league where the best American football players in the world get to show their talents. ',
         image: 'https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg',
         founded: '1920', 
         teams: '32'
+    }, {
+        name: 'NHL', 
+        description: 'The NHL, short for the National Hockey League, is a professional hockey league where the best hockey players in the world get to show off their talents.',
+        image: 'https://logos-world.net/wp-content/uploads/2021/09/NHL-Logo.png',
+        founded: '1917',
+        teams: '32',
+    }, {
+        name: 'MLB', 
+        description: 'The MLB, short for Major League Baseball, is a professional baseball league where the best baseball players in the world get to show off their talents.',
+        image: 'http://cdn.shopify.com/s/files/1/0480/9470/7866/collections/ef26964ae31041325cd9672682c01534.jpg?v=1646869133',
+        founded: '1876',
+        teams: '30',
     }
 ]
